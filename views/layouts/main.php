753315336157456db87fbf63eb0429c9104baa83
Alexandra Travkina <hello@fixel.ru>
2021-08-16 17:08:59 +0700

    Upload

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -21,8 +21,6 @@
         <?= $this->render('head'); ?>
     </head>
     <body>
-        <svg class="svg-filter" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1"><defs><filter id="filter-icon"><feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur"></feGaussianBlur><feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo"></feColorMatrix><feComposite in="SourceGraphic" in2="goo" operator="atop"></feComposite></filter></defs></svg>
-
         <?php $this->beginBody(); ?>
 
         <div class="sidebar-button">
