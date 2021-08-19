3da6baf8d6426ea171902f7bd74528fcf5341bcc
insoln <insoln@users.noreply.github.com>
2021-08-19 15:41:00 +0200

    Update _layout.php
    Conditional replacement of sidebar widgets

--- views/directory/directory/_layout.php
+++ views/directory/directory/_layout.php
@@ -1,8 +1,6 @@
 <?php
 
 use humhub\modules\directory\widgets\Menu;
-use humhub\modules\directory\widgets\Sidebar;
-use humhub\widgets\FooterMenu;
 
 \humhub\assets\JqueryKnobAsset::register($this);
 ?>
@@ -10,14 +8,15 @@ use humhub\widgets\FooterMenu;
 <div class="container">
     <div class="row">
         <div class="col-md-2">
-            <?= Menu::widget(); ?>
+            <?php if (class_exists('\humhub\modules\directory\widgets\MenuUsers')): ?>
+                <?=\humhub\modules\directory\widgets\MenuUsers::widget()?>
+                <?=\humhub\modules\directory\widgets\MenuSpaces::widget()?>
+            <?php else: ?>
+                <?=Menu::widget()?>
+            <?php endif; ?>
         </div>
-        <div class="col-md-7">
-            <?= $content; ?>
-        </div>
-        <div class="col-md-3">
-            <?= Sidebar::widget(); ?>
-            <?= FooterMenu::widget(['location' => FooterMenu::LOCATION_SIDEBAR]); ?>
+        <div class="col-md-10">
+            <?=$content;?>
         </div>
     </div>
 </div>
